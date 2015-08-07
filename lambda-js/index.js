618e50246b398e19a4353f60379052bb1354412d
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2015-08-07 15:02:03 +0900

    fix lambda

--- lambda-js/index.js
+++ lambda-js/index.js
@@ -44,4 +44,4 @@ exports.hander = function(event, context) {
 };
 
 // please commented out when release
-exports.hander({'regId' : 'APA91bF-Y8foAtDoSkM2P6T0Yw7s6w01_OS_6_2QnMoFtpmKHFEBXVIedkya8N3HhxN9_mG02bpo4YDHkCj066SUstytAyIJyMh5Ky8udjtQwFVrDErYSYtp7OItGTcRwvkC-xn-A2PXrgeaM_H_Q8NOhtwc3q6-4A'});
+// exports.hander({'regId' : 'APA91bF-Y8foAtDoSkM2P6T0Yw7s6w01_OS_6_2QnMoFtpmKHFEBXVIedkya8N3HhxN9_mG02bpo4YDHkCj066SUstytAyIJyMh5Ky8udjtQwFVrDErYSYtp7OItGTcRwvkC-xn-A2PXrgeaM_H_Q8NOhtwc3q6-4A'});
